4131245806f3de26cb28421bc2141062bcf57dd1
Hiep Nguyen Duy <duyhiepdt1@yahoo.com>
2020-10-07 15:21:58 +0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-uploads-vn"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://xzc6ijyan7.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_eQHab3Axm",
+      APP_CLIENT_ID: "417oo7gh1f7toibaea3ascj8tk",
+      IDENTITY_POOL_ID: "us-east-2:13304030-e7a0-4c16-9baa-74fcd606a1e5"
+    }
+  };
