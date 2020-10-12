afa7794528bf3ebc36fe42ea83fc111a4fc9131a
Hiep Nguyen Duy <duyhiepdt1@yahoo.com>
2020-10-12 14:07:27 +0700

    Set isLocal=false - enable Sentry log

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,7 +1,8 @@
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 
-const isLocal = process.env.NODE_ENV === "development";
+// const isLocal = process.env.NODE_ENV === "development";
+const isLocal = false;
 
 export function initSentry() {
   if (isLocal) {
@@ -45,6 +46,6 @@ export function onError(error) {
   }
 
   logError(error, errorInfo);
-  
+
   alert(message);
 }
